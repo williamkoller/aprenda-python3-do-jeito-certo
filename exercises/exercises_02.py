1e5941540f3b189466b7dbdee865e8b3f23ab56c
william koller <wkoller25@gmail.com>
2021-12-09 22:54:17 -0300

    feat: add exercise 02

---
+++ exercises/exercises_02.py
@@ -0,0 +1,9 @@
+# Um comentario, assim voce poder ler seu programa mais tarde.
+# Qualquer coisa depois de # eh ignorada pelo python.
+
+print("I could have code like this.") # e o comentario depois eh ignorado
+
+# Voce tambem pode user um tomentario para "desabiltar" um codigo
+# print("This won't run.")
+
+print("This will run.")
